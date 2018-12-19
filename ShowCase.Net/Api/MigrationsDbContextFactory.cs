783b4d1551808247ccc5a57d8e6e4fc32eae7c33
AminSaghi <aminsaghi@chmail.ir>
2018-12-19 19:00:47 +0330

    Adding MigrationsDbContextFactory to project ShowCase.Api.

---
+++ ShowCase.Net/Api/MigrationsDbContextFactory.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using ShowCase.Data.DbContexts;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShowCase.Api
+{
+    public class MigrationsDbContextFactory
+    {
+        public DataDbContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var builder = new DbContextOptionsBuilder<DataDbContext>();
+            var connectionString = configuration.GetConnectionString("DataConnection");
+            builder.UseSqlite(connectionString);
+
+            return new DataDbContext(builder.Options);
+        }
+    }
+}
