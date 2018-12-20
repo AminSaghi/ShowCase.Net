da95db46d52f99562d7e312916ecb9832062de72
AminSaghi <aminsaghi@chmail.ir>
2018-12-20 18:37:30 +0330

    Fixing namespaces.

--- ShowCase.Net/Api/Startup.cs
+++ ShowCase.Net/Api/Startup.cs
@@ -11,7 +11,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ShowCase.Data.DbContexts;
 
-namespace Api
+namespace ShowCase.Api
 {
     public class Startup
     {
