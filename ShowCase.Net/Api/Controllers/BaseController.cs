fdbecb5633933450b357471bfe604315f54d37a4
AminSaghi <aminsaghi@chmail.ir>
2018-12-20 18:36:15 +0330

    Adding BaseController.

---
+++ ShowCase.Net/Api/Controllers/BaseController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShowCase.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [Produces("application/json")]
+    // [PreventCsrfAngular]
+    public class BaseController : Controller
+    {
+    }
+}
