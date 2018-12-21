2ccf2e207116fd827b91e0e134764ff9259add0d
AminSaghi <aminsaghi@gmail.com>
2018-12-21 13:41:19 +0330

    Implementing object mapping functions in Startup.cs.

--- ShowCase.Net/Api/Startup.cs
+++ ShowCase.Net/Api/Startup.cs
@@ -10,7 +10,9 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using ShowCase.Data.DbContexts;
+using ShowCase.Data.Models.ApiModels.Feature;
 using ShowCase.Data.Models.ApiModels.Page;
+using ShowCase.Data.Models.ApiModels.Project;
 using ShowCase.Data.Models.Entities;
 
 namespace ShowCase.Api
@@ -63,7 +65,73 @@ namespace ShowCase.Api
                 .Map(dest => dest.published, src => src.Published)
                 .Map(dest => dest.children, src => src.Children);
 
-            //TODO: mapping for PageApiModel.
+            TypeAdapterConfig<Page, PageApiModel>
+                .ForType()
+                .Map(dest => dest.id, src => src.Id)
+                .Map(dest => dest.orderIndex, src => src.OrderIndex)
+                .Map(dest => dest.title, src => src.Title)
+                .Map(dest => dest.slug, src => src.Slug)
+                .Map(dest => dest.content, src => src.Content)
+                .Map(dest => dest.createDateTime,
+                    src => src.CreateDateTime.LocalDateTime.ToString("yyyy-MM-dd HH:mm"))
+                .Map(dest => dest.updateDateTime,
+                    src => src.UpdateDateTime.LocalDateTime.ToString("yyyy-MM-dd HH:mm"))
+                .Map(dest => dest.published, src => src.Published)
+                .Map(dest => dest.children, src => src.Children);
+
+            #endregion
+
+            #region Project
+
+            TypeAdapterConfig<Project, ListProjectsApiModel>
+                .ForType()
+                .Map(dest => dest.id, src => src.Id)
+                .Map(dest => dest.orderIndex, src => src.OrderIndex)
+                .Map(dest => dest.title, src => src.Title)
+                .Map(dest => dest.slug, src => src.Slug)
+                .Map(dest => dest.imageUrl, src => src.ImageUrl)
+                .Map(dest => dest.published, src => src.Published);                
+
+            TypeAdapterConfig<Project, ProjectApiModel>
+                .ForType()
+                .Map(dest => dest.id, src => src.Id)
+                .Map(dest => dest.orderIndex, src => src.OrderIndex)
+                .Map(dest => dest.title, src => src.Title)
+                .Map(dest => dest.slug, src => src.Slug)
+                .Map(dest => dest.imageUrl, src => src.ImageUrl)               
+                .Map(dest => dest.published, src => src.Published)
+                .Map(dest => dest.features, src => src.Features);
+
+            #endregion
+
+            #region Feature
+
+            TypeAdapterConfig<Feature, ListFeaturesApiModel>
+                .ForType()
+                .Map(dest => dest.id, src => src.Id)
+                .Map(dest => dest.orderIndex, src => src.OrderIndex)
+                .Map(dest => dest.title, src => src.Title)
+                .Map(dest => dest.slug, src => src.Slug)
+                .Map(dest => dest.updateDateTime,
+                    src => src.UpdateDateTime.LocalDateTime.ToString("yyyy-MM-dd HH:mm"))
+                .Map(dest => dest.published, src => src.Published)
+                .Map(dest => dest.project, src => src.Project)
+                .Map(dest => dest.children, src => src.Children);
+
+            TypeAdapterConfig<Feature, FeatureApiModel>
+                .ForType()
+                .Map(dest => dest.id, src => src.Id)
+                .Map(dest => dest.orderIndex, src => src.OrderIndex)
+                .Map(dest => dest.title, src => src.Title)
+                .Map(dest => dest.slug, src => src.Slug)
+                .Map(dest => dest.content, src => src.Content)
+                .Map(dest => dest.createDateTime,
+                    src => src.CreateDateTime.LocalDateTime.ToString("yyyy-MM-dd HH:mm"))
+                .Map(dest => dest.updateDateTime,
+                    src => src.UpdateDateTime.LocalDateTime.ToString("yyyy-MM-dd HH:mm"))
+                .Map(dest => dest.published, src => src.Published)
+                .Map(dest => dest.project, src => src.Project)
+                .Map(dest => dest.children, src => src.Children);
 
             #endregion
         }
