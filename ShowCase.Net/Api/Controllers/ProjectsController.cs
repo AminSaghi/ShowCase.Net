596689be2a7cf6d33b1db4fd9dfae5a39608c726
AminSaghi <aminsaghi@gmail.com>
2018-12-21 17:09:52 +0330

    Implementing ProjectManager in ShowCase.Service and move CRUD logic from ProjectsController to ProjectManager.

--- ShowCase.Net/Api/Controllers/ProjectsController.cs
+++ ShowCase.Net/Api/Controllers/ProjectsController.cs
@@ -1,68 +1,43 @@
 ﻿using Mapster;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
-using System;
-using System.Linq;
 using System.Threading.Tasks;
 
-using ShowCase.Data.DbContexts;
-using ShowCase.Data.Models.ApiModels.Page;
 using ShowCase.Data.Models.ApiModels.Project;
-using ShowCase.Data.Models.Entities;
 using ShowCase.Util.StaticClasses;
+using ShowCase.Service.DataManagers;
 
 namespace ShowCase.Api.Controllers
 {
     public class ProjectsController : BaseController
     {
-        DataDbContext db;
-
-        public ProjectsController(DataDbContext context)
-        {            
-            db = context;
-        }
-
         #region CRUD
 
         [HttpGet]
         public async Task<IActionResult> GetPublishedProjects()
         {
-            try
+            var getProjectsResult = await ProjectManager.Instance.GetProjectsAsync();
+            if (getProjectsResult.Success)
             {
-                var projects = await db.Projects
-                    .Where(p => p.Published)
-                    .OrderBy(p => p.OrderIndex)
-                    .ToListAsync();
-                
-                return Ok(projects.ToArray().Adapt<ListProjectsApiModel[]>());
+                return Ok(getProjectsResult.ReturningValue.ToArray().Adapt<ListProjectsApiModel[]>());
             }
-            catch (Exception ex)
+            else
             {
-                return StatusCode(500, ex.Message);
+                return StatusCode(500, getProjectsResult.Message);
             }
         }
 
         [HttpGet("{id}")]
         public async Task<IActionResult> GetPublishedProject(int id)
         {
-            try
+            var getProjectResult = await ProjectManager.Instance.GetProjectAsync(id);
+            if (getProjectResult.Success)
             {
-                var project = await db.Projects
-                    .FirstOrDefaultAsync(p => p.Id == id && p.Published);
-
-                if (project != null)
-                {                    
-                    return Ok(project.Adapt<ProjectApiModel>());
-                }
-                else
-                {
-                    return NotFound(ReturningMessages.NotFound(project));
-                }
+                return Ok(getProjectResult.ReturningValue.Adapt<ProjectApiModel>());
             }
-            catch (Exception ex)
+            else
             {
-                return StatusCode(500, ex.Message);
+                return StatusCode(500, getProjectResult.Message);
             }
         }
 
@@ -72,30 +47,14 @@ namespace ShowCase.Api.Controllers
         {
             if (ModelState.IsValid)
             {
-                try
+                var createProjectResult = await ProjectManager.Instance.CreateProjectAsync(model);
+                if (createProjectResult.Success)
                 {
-                    var orderIndex = await db.Projects.MaxAsync(p => p.OrderIndex);
-
-                    var now = DateTimeOffset.Now;
-
-                    var project = new Project
-                    {                     
-                        OrderIndex = ++orderIndex,
-                        Title = model.title,
-                        Slug = model.slug,
-                        ImageUrl = model.imageUrl,
-
-                        Published = false
-                    };
-
-                    db.Projects.Add(project);
-                    await db.SaveChangesAsync();
-                    
-                    return Ok(ReturningMessages.CreateSuccessful(project));
+                    return Ok(createProjectResult.Message);
                 }
-                catch (Exception ex)
+                else
                 {
-                    return StatusCode(500, ex.Message);
+                    return StatusCode(createProjectResult.StatusCode, createProjectResult.Message);
                 }
             }
             else
@@ -110,32 +69,14 @@ namespace ShowCase.Api.Controllers
         {
             if (ModelState.IsValid)
             {
-                try
+                var updateProjectResult = await ProjectManager.Instance.UpdateProjectAsync(model);
+                if (updateProjectResult.Success)
                 {
-                    var project = await db.Projects                        
-                        .FirstOrDefaultAsync(p => p.Id == model.id);
-
-                    if (project != null)
-                    {
-                        project.OrderIndex = model.orderIndex;
-                        project.Title = model.title;
-                        project.Slug = model.slug;
-
-                        project.Published = model.published;
-
-                        db.Entry(project).State = EntityState.Modified;
-                        await db.SaveChangesAsync();
-                        
-                        return Ok(ReturningMessages.UpdateSuccessful(project));
-                    }
-                    else
-                    {
-                        return NotFound(ReturningMessages.NotFound(project));
-                    }
+                    return Ok(updateProjectResult.Message);
                 }
-                catch (Exception ex)
+                else
                 {
-                    return StatusCode(500, ex.Message);
+                    return StatusCode(updateProjectResult.StatusCode, updateProjectResult.Message);
                 }
             }
             else
@@ -148,26 +89,14 @@ namespace ShowCase.Api.Controllers
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteProject(int id)
         {
-            try
+            var deleteProjectResult = await ProjectManager.Instance.DeleteProjectAsync(id);
+            if (deleteProjectResult.Success)
             {
-                var project = await db.Projects
-                    .FirstOrDefaultAsync(p => p.Id == id);
-
-                if (project != null)
-                {
-                    db.Projects.Remove(project);
-                    await db.SaveChangesAsync();
-
-                    return Ok(ReturningMessages.DeleteSuccessful(project));
-                }
-                else
-                {
-                    return NotFound(ReturningMessages.NotFound(project));
-                }
+                return Ok(deleteProjectResult.Message);
             }
-            catch (Exception ex)
+            else
             {
-                return StatusCode(500, ex.Message);
+                return StatusCode(deleteProjectResult.StatusCode, deleteProjectResult.Message);
             }
         }
 
---
+++ ShowCase.Net/ShowCase.Service/DataManagers/ProjectManager.cs
@@ -0,0 +1,224 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShowCase.Data.Contracts.OperationResults;
+using ShowCase.Data.DbContexts;
+using ShowCase.Data.Models.ApiModels.Project;
+using ShowCase.Data.Models.Entities;
+using ShowCase.Util.StaticClasses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShowCase.Service.DataManagers
+{
+    public class ProjectManager
+    {
+        #region Singleton
+
+        private static readonly ProjectManager instance = new ProjectManager(DataDbContext.CreateContext());
+
+        static ProjectManager()
+        {
+        }
+
+        private ProjectManager(DataDbContext dbcontext)
+        {
+            db = dbcontext;
+        }
+
+        public static ProjectManager Instance => instance;
+
+        #endregion
+
+        private DataDbContext db;
+
+        #region CRUD
+
+        public async Task<CrudOperationResult<List<Project>>> GetProjectsAsync(bool onlyPublished = true)
+        {
+            try
+            {
+                var projects = await db.Projects
+                    .Where(p => p.Published == onlyPublished)
+                    .OrderBy(p => p.OrderIndex)           
+                    .ToListAsync();
+
+                return new CrudOperationResult<List<Project>>
+                {
+                    Success = true,
+                    StatusCode = 200,
+                    ReturningValue = projects
+                };
+            }
+            catch (Exception ex)
+            {
+                return new CrudOperationResult<List<Project>>
+                {
+                    Success = false,
+                    StatusCode = 500,
+                    Message = ex.Message
+                };
+            }
+        }
+
+        public async Task<CrudOperationResult<Project>> GetProjectAsync(int id, bool onlyPublished = true)
+        {
+            try
+            {
+                var project = await db.Projects
+                     .FirstOrDefaultAsync(p => p.Id == id && p.Published == onlyPublished);
+
+                return new CrudOperationResult<Project>
+                {
+                    Success = true,
+                    StatusCode = 200,
+                    ReturningValue = project
+                };
+            }
+            catch (Exception ex)
+            {
+                return new CrudOperationResult<Project>
+                {
+                    Success = false,
+                    StatusCode = 500,
+                    Message = ex.Message
+                };
+            }
+        }
+
+        public async Task<CrudOperationResult<Project>> CreateProjectAsync(CreateProjectApiModel model)
+        {
+            try
+            {               
+                var orderIndex = await db.Projects.MaxAsync(p => p.OrderIndex);
+
+                var now = DateTimeOffset.Now;
+
+                var project = new Project
+                {                    
+                    OrderIndex = ++orderIndex,
+                    Title = model.title,
+                    Slug = model.slug,
+                    ImageUrl = model.imageUrl,  
+
+                    Published = false
+                };
+
+                db.Projects.Add(project);
+                await db.SaveChangesAsync();
+
+                return new CrudOperationResult<Project>
+                {
+                    Success = true,
+                    StatusCode = 200,
+                    Message = ReturningMessages.CreateSuccessful(project),
+                    ReturningValue = project
+                };
+            }
+            catch (Exception ex)
+            {
+                return new CrudOperationResult<Project>
+                {
+                    Success = false,
+                    StatusCode = 500,
+                    Message = ex.Message
+                };
+            }
+        }
+
+        public async Task<CrudOperationResult<Project>> UpdateProjectAsync(EditProjectApiModel model)
+        {
+            try
+            {
+                var getProjectResult = await GetProjectAsync(model.id, false);            
+                if (getProjectResult.Success)
+                {
+                    var project = getProjectResult.ReturningValue;                    
+
+                    project.OrderIndex = model.orderIndex;
+                    project.Title = model.title;
+                    project.Slug = model.slug;                    
+                    project.Published = model.published;
+
+                    db.Entry(getProjectResult).State = EntityState.Modified;
+                    await db.SaveChangesAsync();
+
+                    return new CrudOperationResult<Project>
+                    {
+                        Success = true,
+                        StatusCode = 200,
+                        ReturningValue = project,
+                        Message = ReturningMessages.UpdateSuccessful(project)
+                    };                    
+                }
+                else
+                {
+                    return new CrudOperationResult<Project>
+                    {
+                        Success = false,
+                        StatusCode = 404,
+                        Message = ReturningMessages.NotFound(getProjectResult.ReturningValue)
+                    };                 
+                }
+            }
+            catch (Exception ex)
+            {
+                return new CrudOperationResult<Project>
+                {
+                    Success = false,
+                    StatusCode = 500,
+                    Message = ex.Message
+                };                
+            }
+        }
+
+        public async Task<CrudOperationResult<Project>> DeleteProjectAsync(int id)
+        {
+            try
+            {
+                var getProjectResult = await GetProjectAsync(id, false);
+                if (getProjectResult.Success)
+                {
+                    var project = getProjectResult.ReturningValue;
+
+                    db.Projects.Remove(project);
+                    await db.SaveChangesAsync();
+
+                    return new CrudOperationResult<Project>
+                    {
+                        Success = true,
+                        StatusCode = 200,
+                        Message = ReturningMessages.DeleteSuccessful(project)
+                    };                    
+                }
+                else
+                {
+                    return new CrudOperationResult<Project>
+                    {
+                        Success = false,
+                        StatusCode = 404,
+                        Message = ReturningMessages.NotFound(getProjectResult.ReturningValue)
+                    };                    
+                }
+            }
+            catch (Exception ex)
+            {
+                return new CrudOperationResult<Project>
+                {
+                    Success = false,
+                    StatusCode = 500,
+                    Message = ex.Message
+                };
+            }
+        }
+
+        #endregion
+
+        #region Private Methods
+
+
+
+        #endregion
+    }
+}
