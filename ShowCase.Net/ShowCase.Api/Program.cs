f6375c25a026e91c471400758d83c7437ca4dbbb
AminSaghi <aminsaghi@gmail.com>
2018-12-22 18:57:51 +0330

    Adding migrations support.

--- ShowCase.Net/ShowCase.Api/Program.cs
+++ ShowCase.Net/ShowCase.Api/Program.cs
@@ -5,21 +5,41 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using ShowCase.Data.DbContexts;
 
 namespace ShowCase.Api
 {
-    public class Program
+    public static class Program
     {
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            BuildWebHost(args)
+                .MigrateDatabase()
+                .Run();
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
+
+        public static IWebHost MigrateDatabase(this IWebHost webHost)
+        {
+            var serviceScopeFactory = (IServiceScopeFactory)webHost.Services.GetService(typeof(IServiceScopeFactory));
+
+            using (var scope = serviceScopeFactory.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var pdb = services.GetRequiredService<DataDbContext>();                
+
+                pdb.Database.Migrate();            
+            }
+
+            return webHost;
+        }
     }
 }
