d31931c2d706db2a26932111d15c650bd6dca5d2
AminSaghi <aminsaghi@gmail.com>
2018-12-23 21:32:38 +0330

    Implementing CreateEditFeatureComponent.

--- ShowCase.Net/ShowCase.FrontEnd/angular/src/app/components/feature/feature.module.ts
+++ ShowCase.Net/ShowCase.FrontEnd/angular/src/app/components/feature/feature.module.ts
@@ -4,6 +4,11 @@ import { CommonModule } from '@angular/common';
 import { ConfirmDialogModule } from 'primeng/confirmdialog';
 import { TreeTableModule } from 'primeng/treetable';
 import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { SpinnerModule } from 'primeng/spinner';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { EditorModule } from 'primeng/editor';
 
 import { FeatureRoutingModule } from './feature-routing.module';
 import { CreateEditFeatureComponent } from './create-edit-feature/create-edit-feature.component';
@@ -16,6 +21,11 @@ import { ListFeaturesComponent } from './list-features/list-features.component';
     ConfirmDialogModule,
     TreeTableModule,
     ButtonModule,
+    DropdownModule,
+    InputTextModule,
+    SpinnerModule,
+    ToggleButtonModule,
+    EditorModule,
 
     FeatureRoutingModule
   ],
