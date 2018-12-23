21e75596f077d09c81b18f99398a5194b130d948
AminSaghi <aminsaghi@gmail.com>
2018-12-23 21:09:32 +0330

    Implement ListFeaturesComponent.

--- ShowCase.Net/ShowCase.FrontEnd/angular/src/app/components/feature/feature.module.ts
+++ ShowCase.Net/ShowCase.FrontEnd/angular/src/app/components/feature/feature.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { TreeTableModule } from 'primeng/treetable';
+import { ButtonModule } from 'primeng/button';
+
 import { FeatureRoutingModule } from './feature-routing.module';
 import { CreateEditFeatureComponent } from './create-edit-feature/create-edit-feature.component';
 import { ListFeaturesComponent } from './list-features/list-features.component';
@@ -8,6 +12,11 @@ import { ListFeaturesComponent } from './list-features/list-features.component';
 @NgModule({
   imports: [
     CommonModule,
+
+    ConfirmDialogModule,
+    TreeTableModule,
+    ButtonModule,
+
     FeatureRoutingModule
   ],
   declarations: [CreateEditFeatureComponent, ListFeaturesComponent]
