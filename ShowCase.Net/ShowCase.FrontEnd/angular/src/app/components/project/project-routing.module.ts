9a7ad011907d825372fccd6df0e02f7131b08037
AminSaghi <aminsaghi@gmail.com>
2018-12-24 19:04:58 +0330

    Implementing project-routing.

--- ShowCase.Net/ShowCase.FrontEnd/angular/src/app/components/project/project-routing.module.ts
+++ ShowCase.Net/ShowCase.FrontEnd/angular/src/app/components/project/project-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ListProjectsComponent } from './list-projects/list-projects.component';
+import { CreateEditProjectComponent } from './create-edit-project/create-edit-project.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ListProjectsComponent
+  },
+  {
+    path: 'ce',
+    component: CreateEditProjectComponent
+  },
+  {
+    path: 'ce/:id',
+    component: CreateEditProjectComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
