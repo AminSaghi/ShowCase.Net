df9170cd709e3bf931a3f1138c5155a2e6595973
AminSaghi <aminsaghi@gmail.com>
2018-12-24 19:02:32 +0330

    Implementing CreateEditProjectComponent.

--- ShowCase.Net/ShowCase.FrontEnd/angular/src/app/components/project/create-edit-project/create-edit-project.component.ts
+++ ShowCase.Net/ShowCase.FrontEnd/angular/src/app/components/project/create-edit-project/create-edit-project.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+import { FormBuilder, FormControl, Validators } from '@angular/forms';
+
+import { Project } from 'src/app/api-client/models';
+import { ProjectService } from 'src/app/api-client';
 
 @Component({
   selector: 'app-create-edit-project',
@@ -6,10 +12,84 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-edit-project.component.css']
 })
 export class CreateEditProjectComponent implements OnInit {
+  editMode = false;
+  public cardHeaderText = 'Create new Project';
+  public commandButtonText = 'Create';
+  public commandButtonClass = 'ui-button-success';
+  public commandButtonIcon = 'pi pi-plus';
 
-  constructor() { }
+  projectForm;
+  projects: Project[];
+  projectModel: Project;
 
-  ngOnInit() {
+  constructor(private activatedRoute: ActivatedRoute,
+    private formBuilder: FormBuilder,
+    private projectService: ProjectService,
+    private location: Location) {
+
+    this.projectForm = formBuilder.group({
+      id: new FormControl(this.projectModel.id),
+      orderIndex: new FormControl(this.projectModel.orderIndex),
+      title: new FormControl(this.projectModel.title, Validators.required),
+      slug: new FormControl(this.projectModel.slug, Validators.required),
+      description: new FormControl(this.projectModel.description),
+      published: new FormControl(this.projectModel.published)
+    });
   }
 
+  ngOnInit() {
+    this.projectService.getProjects().subscribe(Projects => {
+      this.projects = Projects;
+
+      const id = this.activatedRoute.snapshot.params['id'];
+      if (id) {
+        this.projectService.getProject(id).subscribe(project => {
+          this.projectForm.controls['id'].setValue(project.id);
+          this.projectForm.controls['orderIndex'].setValue(project.orderIndex);
+          this.projectForm.controls['title'].setValue(project.title);
+          this.projectForm.controls['slug'].setValue(project.slug);
+          this.projectForm.controls['description'].setValue(project.description);
+          this.projectForm.controls['published'].setValue(project.published);
+
+          this.editMode = true;
+          this.cardHeaderText = 'Edit Project';
+          this.commandButtonText = 'Save';
+          this.commandButtonClass = 'ui-button-warning';
+          this.commandButtonIcon = 'pi pi-check';
+        }, error => {
+          this.goBack();
+        });
+      }
+    });
+  }
+
+  createProject() {
+    if (this.projectForm.valid) {
+      const formValue = this.projectForm.value;
+      this.projectService.createProject(formValue).subscribe();
+    } else {
+      Object.keys(this.projectForm.controls).forEach(field => {
+        const control = this.projectForm.get(field);
+        control.markAsTouched({ onlySelf: true });
+      });
+    }
+  }
+
+  editProject() {
+    if (this.projectForm.valid) {
+      const formValue = this.projectForm.value;
+      this.projectService.editProject(formValue).subscribe();
+    } else {
+      Object.keys(this.projectForm.controls).forEach(field => {
+        const control = this.projectForm.get(field);
+        control.markAsTouched({ onlySelf: true });
+      });
+    }
+  }
+
+  isInvalid(controlName) {
+    return this.projectForm.controls[controlName].invalid && this.projectForm.controls[controlName].touched;
+  }
+
+  goBack() { this.location.back(); }
 }
