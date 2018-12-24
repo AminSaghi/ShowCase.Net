13613a865f0677574a6585897d55ac9088bb5029
AminSaghi <aminsaghi@gmail.com>
2018-12-24 17:55:14 +0330

    Implementing ListProjectsComponent.

--- ShowCase.Net/ShowCase.FrontEnd/angular/src/app/components/project/list-projects/list-projects.component.ts
+++ ShowCase.Net/ShowCase.FrontEnd/angular/src/app/components/project/list-projects/list-projects.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { ConfirmationService } from 'primeng/api';
+
+import { ProjectService } from 'src/app/api-client';
 
 @Component({
   selector: 'app-list-projects',
@@ -7,9 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListProjectsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private projectService: ProjectService,
+    private router: Router,
+    private confirmationService: ConfirmationService) { }
+
+  public projects = [];
 
   ngOnInit() {
+    this.getProjects();
+  }
+
+  getProjects() {
+    this.projectService.getProjects().subscribe(response => {
+      this.projects = response;
+    });
+  }
+
+  confirmDelete(id: number) {
+    this.confirmationService.confirm({
+      message: 'Are you sure that you want to delete this Project?',
+      accept: () => {
+        this.deleteProject(id);
+      }
+    });
+  }
+
+  deleteProject(projectId: number) {
+    this.projectService.deleteProject(projectId).subscribe(() => {
+      this.projects = this.projects.filter(i => i.id !== projectId);
+    });
   }
 
 }
--- ShowCase.Net/ShowCase.FrontEnd/angular/src/app/components/project/project.module.ts
+++ ShowCase.Net/ShowCase.FrontEnd/angular/src/app/components/project/project.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+import { DropdownModule } from 'primeng/dropdown';
+import { InputTextModule } from 'primeng/inputtext';
+import { SpinnerModule } from 'primeng/spinner';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { EditorModule } from 'primeng/editor';
+
 import { ProjectRoutingModule } from './project-routing.module';
 import { ListProjectsComponent } from './list-projects/list-projects.component';
 import { CreateEditProjectComponent } from './create-edit-project/create-edit-project.component';
-import { ListFeaturesComponent } from './list-features/list-features.component';
 
 @NgModule({
   imports: [
     CommonModule,
+
+    ConfirmDialogModule,
+    TableModule,
+    ButtonModule,
+    DropdownModule,
+    InputTextModule,
+    SpinnerModule,
+    ToggleButtonModule,
+    EditorModule,
+
     ProjectRoutingModule
   ],
-  declarations: [ListProjectsComponent, CreateEditProjectComponent, ListFeaturesComponent]
+  declarations: [ListProjectsComponent, CreateEditProjectComponent]
 })
 export class ProjectModule { }
