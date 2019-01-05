eecef4cb80135fa8b7743b47190f63dfa279331e
AminSaghi <aminsaghi@gmail.com>
2019-01-05 11:50:01 +0330

    Adding DomSanitizer.

--- ShowCase.Net/ShowCase.FrontEnd/angular/src/app/components/home/page/page.component.ts
+++ ShowCase.Net/ShowCase.FrontEnd/angular/src/app/components/home/page/page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
+import { DomSanitizer } from '@angular/platform-browser';
 
 import { PageService } from 'src/app/api-client';
 import { Page } from 'src/app/api-client/models';
@@ -15,7 +16,8 @@ export class PageComponent implements OnInit {
   constructor(
     private activatedRoute: ActivatedRoute,
     private location: Location,
-    private pageService: PageService) { }
+    private pageService: PageService,
+    public sanitizer: DomSanitizer) { }
 
     page: Page;
 
@@ -30,6 +32,10 @@ export class PageComponent implements OnInit {
     } else {
       this.goBack();
     }
+  }
+
+  getPageContent(htmlString: string) {
+    return this.sanitizer.bypassSecurityTrustHtml(htmlString);
   }
 
   goBack() { this.location.back(); }
