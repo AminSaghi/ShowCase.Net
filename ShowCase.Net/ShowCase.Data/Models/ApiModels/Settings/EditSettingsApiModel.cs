3d61e2df0781db0440240590ac26f63173a25d90
AminSaghi <aminsaghi@gmail.com>
2019-01-05 12:43:33 +0330

    Creating Settings api models.

---
+++ ShowCase.Net/ShowCase.Data/Models/ApiModels/Settings/EditSettingsApiModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShowCase.Data.Models.ApiModels.Settings
+{
+    public class EditSettingsApiModel
+    {
+        public string logoUrl { get; set; }
+        public string footerContent { get; set; }
+    }
+}
