3a09b0d2eb846ec3e0472dce396084d94cb71aaa
AminSaghi <aminsaghi@gmail.com>
2019-01-07 10:52:52 +0330

    Updating routings of manage components.

--- ShowCase.Net/ShowCase.FrontEnd/angular/src/app/components/manage/manage-routing.module.ts
+++ ShowCase.Net/ShowCase.FrontEnd/angular/src/app/components/manage/manage-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ManageHomeComponent } from './manage-home/manage-home.component';
+import { SettingsComponent } from './settings/settings.component';
+import { AccountComponent } from './account/account.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ManageHomeComponent
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent
+  },
+  {
+    path: 'account',
+    component: AccountComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
