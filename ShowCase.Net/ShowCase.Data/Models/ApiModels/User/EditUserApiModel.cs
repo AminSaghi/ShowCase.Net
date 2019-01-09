ae8bb1aaa5f725c4ff3859763097dda00904022d
AminSaghi <aminsaghi@gmail.com>
2019-01-09 11:30:57 +0330

    Implementing user api models.

---
+++ ShowCase.Net/ShowCase.Data/Models/ApiModels/User/EditUserApiModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ShowCase.Data.Models.ApiModels.User
+{
+    public class EditUserApiModel
+    {
+        [Required]
+        public string id { get; set; }
+
+        [Required]
+        [Display(Name = "Username")]
+        public string userName { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
+        public string email { get; set; }
+    }
+}
